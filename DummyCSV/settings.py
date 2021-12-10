90b195a9bad415c5997e751b40ff2cc4e45ba753
yurasblv <yurasblv.y@gmail.com>
2021-12-10 19:30:48 +0200

    1

--- DummyCSV/settings.py
+++ DummyCSV/settings.py
@@ -84,6 +84,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+CELERY_BROKER_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
